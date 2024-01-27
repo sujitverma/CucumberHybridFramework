872850a8daf68058e56c45a9e936b3e3a7f5a8fc
sujitverma <sujit.cam@gmail.com>
2024-01-27 21:41:38 +0530

    code update

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -1,11 +1,9 @@
 package testrunners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -14,16 +12,10 @@ import org.testng.annotations.DataProvider;
 		plugin = {"pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"timeline:target/test-output/output-thread/"		
-		},
-		monochrome = true,
-		publish = true
+		}
+
 		)
 
-public class MyTestRunner extends AbstractTestNGCucumberTests {
-	@Override
-	@DataProvider(parallel = true)
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
+public class MyTestRunner {
 
 }
