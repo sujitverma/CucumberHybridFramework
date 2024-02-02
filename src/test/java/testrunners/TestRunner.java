62168249d011319e96fb6a779b26390a74654969
sujitverma <sujit.cam@gmail.com>
2024-02-02 21:30:15 +0530

    Removed Parallel run

---
+++ src/test/java/testrunners/TestRunner.java
@@ -0,0 +1,23 @@
+package testrunners;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(plugin = {"pretty",
+						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+						"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+			                        "html:target/cucumber-reports/index.html",
+			                        "json:target/cucumber-reports/Cucumber.json",
+			                        "junit:target/cucumber-reports/Cucumber.xml",
+						"timeline:target/test-output/output-thread/",
+						"rerun:target/failedrun.txt"}, 
+				monochrome = true,
+				glue = {"stepdefinitions", "parallel"},
+				features = { "src/test/resources/parallel/" }
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+	
+}
